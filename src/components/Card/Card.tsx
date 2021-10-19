62f1979ec6ecd5b705c7f15c980350193b5c798d
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-10-19 12:15:27 +0100

    Finish adding types to Card component + small refactor
    The refactor was just changing opacity and setOpacity to animate and
    setAnimate. That seemed more appropriate as scale is also being animated
    at the same time.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,37 +4,39 @@ import { Quote } from "../../types/types";
 import { quotes } from "../../quotes/quotes";
 import createQuotes from "../../utils/createQuoteFile";
 
-const quotesList: Quote[] = createQuotes(quotes);
-
-const colors = [
-  "#735091",
-  "#5a5fa8",
-  "#3d7692",
-  "#518a6e",
-  "#797460",
-  "#ac5959",
-];
-
-let color = 1;
-
-const root = document.documentElement;
-
-function randomNumber(quotes: Quote[]): number {
-  return Math.floor(Math.random() * quotes.length);
-}
-
 function Card(): JSX.Element {
-  const [quote, setQuote] = useState(quotesList[randomNumber(quotesList)]);
-  const [opacity, setOpacity] = useState("");
+  const quotesList: Quote[] = createQuotes(quotes);
 
-  function getNewQuote() {
-    setTimeout(function () {
+  const colors: string[] = [
+    "#735091",
+    "#5a5fa8",
+    "#3d7692",
+    "#518a6e",
+    "#797460",
+    "#ac5959",
+  ];
+
+  let color = 1;
+
+  const root: HTMLElement = document.documentElement;
+
+  function randomNumber(quotes: Quote[]): number {
+    return Math.floor(Math.random() * quotes.length);
+  }
+
+  const [quote, setQuote] = useState<Quote>(
+    quotesList[randomNumber(quotesList)]
+  );
+  const [animate, setAnimate] = useState<string>("");
+
+  function getNewQuote(): void {
+    setTimeout(function (): void {
       setQuote(quotesList[randomNumber(quotesList)]);
     }, 400);
   }
 
-  function transition() {
-    setOpacity("fade-out");
+  function transition(): void {
+    setAnimate("fade-out");
     root.style.setProperty("--primary-color", colors[color]);
     if (color === colors.length - 1) {
       color = 0;
@@ -44,14 +46,14 @@ function Card(): JSX.Element {
   }
 
   useEffect(
-    function () {
-      setOpacity("");
+    function (): void {
+      setAnimate("");
     },
     [quote]
   );
 
   return (
-    <div className={`Card ${opacity}`}>
+    <div className={`Card ${animate}`}>
       <h1>"{quote.quote}"</h1>
       <h4>- {quote.quoter}</h4>
       <div className="buttons-grid">
